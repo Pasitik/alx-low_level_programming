065e94ec3531cba25386f932a8f360b4a8980424
Pasitik <paasitik@gmail.com>
2023-02-17 13:36:13 -0800

    commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,8 +14,11 @@ int main(void)
 	for (x = 0; x <= 9; x++)
 	{
 		putchar(x % 10 + '0');
-		putchar(',');
-		putchar(' ');
+		if (x !=9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 		putchar('\n');
 		return (0);
