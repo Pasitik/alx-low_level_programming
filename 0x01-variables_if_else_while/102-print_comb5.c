130ce3ec10ada0524d913dff3b38b57bda76c24e
Pasitik <paasitik@gmail.com>
2023-02-17 21:13:49 -0800

    0 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,11 +23,9 @@ int main(void)
 			putchar((b/10) + '0');
 			putchar((b%10) + '0');
 			if (a == 98 && b == 99)
-			{
 				continue;
 				putchar(',');
 				putchar(' ');
-			}
 		}
 	}
 	putchar('\n');
