7e4a25295675332e1c99b02248ca8b834a8b40a5
Pasitik <paasitik@gmail.com>
2023-02-17 20:52:07 -0800

    0-99

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,18 +12,18 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = 0; a <= 99; a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			if (b != a)
+			putchar((a/10) + '0');
+			putchar((a%10) + '0');
+			putchar(' ');
+			putchar((b/10) + '0');
+			putchar((b%10) + '0');
+			if (a == 98 && b == 99)
 			{
-				putchar(a);
-				putchar(b);
-				if (a == '8' && b == '9')
-				{
-					continue;
-				}
+				continue;
 				putchar(',');
 				putchar(' ');
 			}
