fed747f00f98aa9060ce6e66ddfdc1d6d85fa622
Pasitik <paasitik@gmail.com>
2023-02-21 15:03:38 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+
+#include "main.h"
+
+/**
+ * print_last_digit - function for lowercase checks
+ * @n: char to be checked
+ * Return: 1 for lowercase and 0 for anything else
+ */
+int print_last_digit(int n)
+{
+	int last_num = n % 10;
+
+	if (n < 0)
+	{
+		last_num = last_num * -1;
+	}
+	putchar(last_num + '0');
+	return (0);
+}
