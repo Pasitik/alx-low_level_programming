94e6bcf21af3c09a6e6c2b262c5301c9ce6fa9af
Pasitik <paasitik@gmail.com>
2023-02-21 13:06:15 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - function for lowercase checks
+ * @n: char to be checked
+ * Return: 1 for lowercase and 0 for anything else
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
