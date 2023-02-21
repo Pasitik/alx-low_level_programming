cb56fd94f49f68bdd1fa1e23074c5de73ed0ed89
Pasitik <paasitik@gmail.com>
2023-02-21 15:56:42 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - function for lowercase checks
+ * @a: first param
+ * @b: second param
+ * Return: 1 for lowercase and 0 for anything else
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
