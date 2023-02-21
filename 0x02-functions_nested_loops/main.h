f955c85a602e899364219f433da0900e0a03a91f
Pasitik <paasitik@gmail.com>
2023-02-21 09:01:45 -0800

    print

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,6 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
-/**
- * _putchar - my putchar func
- *
- * Return
- */
- int _putchar(char text)
-{
-		putchar(text); 
-		return (0);
-}
+
+ int _putchar(char c);
 #endif
