83916b777ef08a07e2c5491c12190eb5ee1b538f
Pasitik <paasitik@gmail.com>
2023-02-23 10:19:24 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			k = j;
+			if (j > 9)
+			{
+				putchar('1');
+				k = j % 10;
+			}
+			_putchar('0' + K);
+		}
+		_putchar('0');
+	}
+}
