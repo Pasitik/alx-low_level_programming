11d5b2ec1faa020892a7ba78fd4c202310807a19
Pasitik <paasitik@gmail.com>
2023-02-23 12:03:22 -0800

    print

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * print_line - check the code
+ *
+ * @n: param
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n < 1)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			putchar('_');
+		}
+		putchar('\n');
+	}
+}
