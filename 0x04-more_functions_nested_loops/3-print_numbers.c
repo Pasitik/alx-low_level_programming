a8d6ef18b511c3c927546d6d4979b92e1df58b00
Pasitik <paasitik@gmail.com>
2023-02-23 09:07:52 -0800

    0 to 1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+
+#include "main.h"
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+int print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+		_putchar(i + '0');
+
+	_putchar('\n')
+	return (0);
+}
