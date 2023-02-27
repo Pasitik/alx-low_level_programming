2b429cef2ec1b51422c992a93fe7f0f727d3e17f
Pasitik <paasitik@gmail.com>
2023-02-27 14:25:56 -0800

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,14 +19,18 @@ void puts_half(char *str)
 	if (len % 2 != 0)
 	{
 		n = (len - 1) / 2;
+		for (i = n; i < len; i++)
+		{
+			_putchar(*(str + i));
+		}
 	}
 	else
 	{
 		n = len / 2;
-	}
-	for (i = n; i < len; i++)
-	{
-		putchar(*(str + i));
+		for (i = n + 1; i < len; i++)
+		{
+			_putchar(*(str + i));
+		}
 	}
 	_putchar('\n');
 }
