7c2fbb1b08518b30458680949a561c7d875428ff
Pasitik <paasitik@gmail.com>
2023-02-27 08:21:59 -0800

    Half

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * @str: param
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int len = 0;
+
+	int i;
+
+	while (*(str + len) != '\0')
+	{
+		if (len == 0 || len % 2 == 0)
+		{
+			putchar(*(str + len));
+		}
+		len++;
+	}
+	_putchar('\n');
+}
