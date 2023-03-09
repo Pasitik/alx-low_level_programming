1561ce77b47868a65d28735d4a95da9d25c40a9d
Pasitik <paasitik@gmail.com>
2023-03-08 19:02:35 -0800

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - check the code
+ *
+ * @x: param1
+ * @y: param2
+ * Return: pow.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (x * _pow_recursion(x, y - 1))
+}
