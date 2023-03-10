992b21d81fe691ace3e9dbf04712d45f10de74c5
Pasitik <paasitik@gmail.com>
2023-03-10 15:27:19 -0800

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - my main func
@@ -9,6 +9,6 @@
  */
 int main(int argc, char* argv[])
 {
-	printf("%s\n",argv[0]);
+	printf ("%s\n", argv[0]);
 	return (0);
 }
