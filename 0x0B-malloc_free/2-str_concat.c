1ae738cf6ed6c63b4bb1156a70290cb4725b1ab4
Pasitik <paasitik@gmail.com>
2023-03-13 16:21:08 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,9 +24,17 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-	if(s1 == NULL && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
-		return NULL;
+		return (NULL);
+	}
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	p = (char *) malloc((sizeof(char) * size) + 1);
