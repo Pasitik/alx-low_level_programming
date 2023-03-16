32637c3b83acccf22b5708bf32c36f58ff3c6561
Pasitik <paasitik@gmail.com>
2023-03-16 06:42:58 -0700

    concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * string_nconcat - function for concat
+ *
+ * @s1: param1
+ * @s2: param2
+ * @n: param3
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p
+	unsigned int lenA, i, j, size;
+
+	while (*(s1 + lenA) != '\0')
+	{
+		lenA++;
+	}
+	size = lenA + n;
+
+	p = (char *) malloc((sizeof(char) * size) + 1);
+
+	while (*(s1 + i) != '\0')
+	{
+		*(p + i) = *(s1 + i);
+		i++;
+	}
+
+	while (i < size && j < n)
+	{
+		j++;
+		i++;
+	}
+
+	*(p + size + 1) = '\0';
+	return (p);
+}
+
