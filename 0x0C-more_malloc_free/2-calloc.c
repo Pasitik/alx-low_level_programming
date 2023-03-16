3633b8140efb95aebc46ad2bb4207717c8775386
Pasitik <paasitik@gmail.com>
2023-03-16 08:53:03 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,16 +13,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	unsigned int i = 0;
 	
-	unsigned int n = nmemb * size;
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	while (i < n)
+	while (i < nmemb)
 	{
 		*(p + i) = 0;
+		i++;
 	}
 	return (p);
 }
