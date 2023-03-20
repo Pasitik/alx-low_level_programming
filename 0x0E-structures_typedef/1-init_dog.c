e4751472be0cff4cd5e14be865cae9ea359cb5ad
Pasitik <paasitik@gmail.com>
2023-03-20 10:21:09 -0700

    structure pointer as param

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,12 +5,13 @@
  *
  * @d: ....
  * @name: ....
+ * @age: ....
  * @owner: ....
  * Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
