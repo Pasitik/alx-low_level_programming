e18a314c1cd1b92bcda184487d4fa2fcdf4a78cc
Pasitik <paasitik@gmail.com>
2023-03-30 15:47:27 -0700

    print

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -17,8 +17,9 @@ typedef struct node
 {
 	char *str;
 	unsigned int len;
-	struct node* next;
-}list_t;
+	struct node *next;
+}
+list_t;
 
 int _putchar(char c);
 size_t print_list(const list_t *h);
