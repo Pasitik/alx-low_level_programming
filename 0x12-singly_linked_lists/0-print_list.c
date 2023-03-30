f353846330476d283cb4bb0d9209be9deb676d85
Pasitik <paasitik@gmail.com>
2023-03-30 15:02:04 -0700

    print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_list - ...
+ *
+ * @h: ...
+ * Return: ...
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
