b0caa342676ae2ffaa32d039801f826348d5cad6
Pasitik <paasitik@gmail.com>
2023-03-30 22:20:57 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * free_list - ...
+  *
+  * @head: ...
+  * Return: ...
+  */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}
+
