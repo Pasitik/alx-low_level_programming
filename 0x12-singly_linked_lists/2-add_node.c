c425503f22f3aa45792adfc4616932b7721495f9
Pasitik <paasitik@gmail.com>
2023-03-30 20:32:51 -0700

    insert

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,49 @@
+# include "lists.h"
+
+/**
+ * add_node -  00....
+ *
+ * @head: ...
+ * @str: ...
+ * Return: ...
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+return (NULL);
+
+}
+
+char *str_copy = strdup(str);
+if (str_copy == NULL)
+{
+free(new_node);
+return (NULL);
+
+}
+new_node->str = str_copy;
+new_node->next = *head;
+*head = new_node;
+
+return (new_node);
+}
+
+/**
+ * _strlen - ...
+ *
+ * @s: ...
+ * Return: ...
+ */
+int _strlen(const char *s)
+{
+int c = 0;
+
+while (*s)
+{
+s++;
+c++;
+}
+return (c);
+}
