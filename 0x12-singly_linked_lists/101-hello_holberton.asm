159ff760ba305b1458fe4a8a4bd51f2d79a2b365
Pasitik <paasitik@gmail.com>
2023-03-30 22:55:54 -0700

    assm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,17 @@
-section .data
-  msg db 'Hello, Holberton', 10, 0
-
 section .text
-  global main
-  
-  extern printf
-  
-  main:
-    push msg
-    mov rdi, format
-    xor eax, eax
-    call printf
-    xor eax, eax
-    ret
-  
-  section .data
-    format db "%s", 10, 0
+global main
 
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
