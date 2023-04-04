65fffe6355ed0c21916c25c57e6262514805bfea
Pasitik <paasitik@gmail.com>
2023-04-04 15:30:30 -0700

    node index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 listint_t *temp = head;
-unsigned int count = 1;
+unsigned int count = 0;
 
 if (head == NULL)
 return (NULL);
