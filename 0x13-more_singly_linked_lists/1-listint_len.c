1704ac4a2e00a88352b623514935c7ab5c165d0f
Pasitik <paasitik@gmail.com>
2023-04-04 08:00:44 -0700

    len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists"
+
+/**
+ * listint_len - ...
+ *
+ * @h: ...
+ * Return: ...
+ */
+size_t listint_len(const listint_t *h)
+{
+const listint_t *temp = h;
+size_t count = 0;
+
+if (h == NULL)
+return (0);
+while (temp != NULL)
+{
+count++;
+temp = temp->next;
+}
+return (count);
+}
