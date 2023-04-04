d7aaaa1cb58a0443a92d9b0c4311afc39d464098
Pasitik <paasitik@gmail.com>
2023-04-04 14:23:17 -0700

    node index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - ...
+ *
+ * @head: ...
+ * @index: ...
+ * Return: ...
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *temp = head;
+unsigned int count = 1;
+
+if (head == NULL)
+return (NULL);
+
+while (count < index)
+{
+temp = temp->next;
+count++;
+}
+return (temp->next);
+}
