20c85cdddb0809820fcbbc78e12d535df6aeb054
Pasitik <paasitik@gmail.com>
2023-04-11 16:30:59 -0700

    endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 
 int _putchar(char c);
 int _strlen(char *s);
+int get_endianness(void);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
