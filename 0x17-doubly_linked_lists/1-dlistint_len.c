a1826da0de717b3e326a902441e13ea139950bb5
Paul Asitik <paasitik@gmail.com>
2023-06-15 11:03:18 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -9,13 +9,13 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *temp = h;
-    size_t count = 0;
-    
-    while (temp != NULL)
-    {
-        temp = temp->next;
-        count++;
-    }
-    return (count);
+	const dlistint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
 }
