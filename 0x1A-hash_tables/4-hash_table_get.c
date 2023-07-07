aba1c658b6b3eb71a4e4739e3102f8c2c87364f9
Paul Asitik <paasitik@gmail.com>
2023-07-07 14:15:26 -0700

    4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value based on key
+ * @ht: hash_table_t pointer
+ * @key: string
+ *
+ * Description: using the key an index is gotten to find
+ * hash_node_t with the same key
+ *
+ * Return: char *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+
+	if(ht->array[index] != NULL && strncmp(ht->array[index]->key, key, ht->size) == 0)
+	{
+		return ht->array[index]->value;
+	}
+	else
+	{
+		return NULL;
+	}
+}
