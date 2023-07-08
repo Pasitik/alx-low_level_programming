026f820659bf2212af059cb85a608599bfbc066d
Paul Asitik <paasitik@gmail.com>
2023-07-07 20:31:17 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	_key = strdup(key);
 
 	if(value)
+	{
 		value_cpy = strdup(value);
+		if (value_cpy == NULL)
+        {
+            free(_key);
+            return 0;
+        }
+	}
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] != NULL)
@@ -30,7 +37,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		hash_node_t *temp = NULL;
 		temp = malloc (sizeof(hash_node_t));
 		if(temp == NULL)
+		{
+			free(_key);
+			free(value_cpy);
 			return (0);
+		}
 		temp->key = _key;
 		temp->value = value_cpy;
 		temp->next = ht->array[index];
@@ -42,7 +53,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		ht->array[index] = malloc(sizeof(hash_node_t *));
 		if(ht->array[index] == NULL)
+		{
+			free(_key);
+			free(value_cpy);
 			return (0);
+		}
 		ht->array[index]->key = _key;
 		ht->array[index]->value = value_cpy;
 		ht->array[index]->next = NULL;
