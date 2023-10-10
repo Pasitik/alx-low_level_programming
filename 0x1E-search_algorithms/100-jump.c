fcabc966938e0c099d94856ab945977e245df27d
Paul Asitik <paasitik@gmail.com>
2023-10-10 12:42:45 -0700

    100-jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,6 +15,9 @@ int jump_search(int *array, size_t size, int value)
 	size_t step = sqrt(size);
 	size_t prev = 0, i = 0;
 
+	if (array == NULL || size == 0)
+		return (-1);
+
 	printf("Value checked array[0] = [%d]\n", array[0]);
 
 	while (array[step] < value && step < size)
