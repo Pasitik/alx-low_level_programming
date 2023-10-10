d81a40bafcdbf8898711a062782223f6e5cc6efb
Paul Asitik <paasitik@gmail.com>
2023-10-10 11:25:41 -0700

    1-binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
+ * binary_search - searches for a value in an array of
  * integers using the Binary search algorithm
  *
  *
@@ -12,22 +12,26 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t mid = 0;
 	size_t l = 0, i = 0;
+	size_t mid = 0;
 	size_t r = size - 1;
 
-	while (l < r)
+	while (l <= r)
 	{
-		mid = (l + r) / 2;
+		mid = l + (r - l) / 2;
+
 		printf("Searching in array: ");
-		for (i = l; i <= r - 1; i++)
+		for (i = l; i <= r; ++i)
 		{
-			printf("%d, ", array[i]);
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
 		}
-		printf("%d\n", array[r]);
+		printf("\n");
 
 		if (value == array[mid])
 		{
+			printf("Found %d at index: %zu\n", value, mid);
 			return (mid);
 		}
 		else if (value < array[mid])
@@ -39,5 +43,6 @@ int binary_search(int *array, size_t size, int value)
 			l = mid + 1;
 		}
 	}
+
 	return (-1);
 }
